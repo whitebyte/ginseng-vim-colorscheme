83bb3c5ffc7e32c1cf16fbd7fa38ed55b568f25c
Anton Pastukhov <caffeine9999@mailbox.org>
2024-10-09 23:02:05 +0300

    colors

--- colors/ginseng.lua
+++ colors/ginseng.lua
@@ -29,11 +29,22 @@ local colors = {
     steel   = "#8898b2"
 }
 
-hi('Normal',          { fg = colors.beige1, bg = colors.grey4 });
+hi('Normal',          { fg = colors.beige1, bg = colors.brown1 });
+hi('Title',           { fg = colors.orange1 });
 hi('Comment',         { fg = colors.grey2 , italic = true });
+hi('Folded',          { fg = colors.grey1, bg = colors.grey4 })
+
+hi('NormalFloat',     { fg = colors.beige2, bg = colors.brown1})
+hi('FloatBorder',     { link = 'WinSeparator' })
+hi('FloatTitle',      { link = 'Title' })
+hi('FloatFooter',     { link = 'Title' })
 
 hi('Search',          { fg = colors.orange1 });
-hi('IncSearch',       { fg = colors.yellow1 });
+hi('IncSearch',       { fg = colors.orange2 });
+
+hi('Pmenu',           { fg = colors.black, bg = colors.beige2 });
+hi('PmenuSel',        { fg = colors.black, bg = colors.orange2 });
+
 
 hi('String',          { fg = colors.grey1 });
 hi('Character',       { fg = colors.grey1 });
@@ -44,6 +55,7 @@ hi('Boolean',         { fg = colors.orange2 });
 hi('Type',            { fg = colors.copper });
 hi('Statement',       { fg = colors.steel });
 hi('Keyword',         { fg = colors.steel });
+hi('Constant',        { fg = colors.orange2 });
 
 hi('Identifier',      { fg = colors.beige3 });
 hi('Function',        { fg = colors.orange1 });
