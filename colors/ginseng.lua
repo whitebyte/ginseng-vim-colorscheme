5a485a7751d362db14204ceeb708baddec82e157
Anton Pastukhov <caffeine9999@mailbox.org>
2024-10-09 23:15:52 +0300

    colors

--- colors/ginseng.lua
+++ colors/ginseng.lua
@@ -22,51 +22,60 @@ local colors = {
     grey2   = "#747474",
     grey3   = "#565656",
     grey4   = "#303032",
+    grey5   = "#181818",
     orange1 = "#df8800",
     orange2 = "#ff9800",
     yellow1 = "#ffffbb",
     copper  = "#ad5640",
-    steel   = "#8898b2"
+    steel   = "#8898b2",
+    purple  = "#927292"
 }
 
-hi('Normal',          { fg = colors.beige1, bg = colors.brown1 });
-hi('Title',           { fg = colors.orange1 });
-hi('Comment',         { fg = colors.grey2 , italic = true });
-hi('Folded',          { fg = colors.grey1, bg = colors.grey4 })
+hi('Normal',              { fg = colors.beige1, bg = colors.brown1 });
+hi('Title',               { fg = colors.orange1 });
+hi('Comment',             { fg = colors.grey2 , italic = true });
+hi('Folded',              { fg = colors.grey1, bg = colors.grey4 })
 
-hi('NormalFloat',     { fg = colors.beige2, bg = colors.brown1})
-hi('FloatBorder',     { link = 'WinSeparator' })
-hi('FloatTitle',      { link = 'Title' })
-hi('FloatFooter',     { link = 'Title' })
+hi('LineNr',              { fg = colors.beige2, bg = colors.black })
 
-hi('Search',          { fg = colors.orange1 });
-hi('IncSearch',       { fg = colors.orange2 });
+hi('NormalFloat',         { fg = colors.beige2, bg = colors.brown1})
+hi('FloatBorder',         { link = 'WinSeparator' })
+hi('FloatTitle',          { link = 'Title' })
+hi('FloatFooter',         { link = 'Title' })
 
-hi('Pmenu',           { fg = colors.black, bg = colors.beige2 });
-hi('PmenuSel',        { fg = colors.black, bg = colors.orange2 });
+hi('Search',              { fg = colors.black, bg = colors.orange1 });
+hi('IncSearch',           { fg = colors.black, bg = colors.orange2 });
 
+hi('Visual',              { fg = colors.black, bg = colors.steel });
 
-hi('String',          { fg = colors.grey1 });
-hi('Character',       { fg = colors.grey1 });
-hi('Number',          { fg = colors.orange1 });
-hi('Float',           { fg = colors.orange1 });
-hi('Boolean',         { fg = colors.orange2 });
+hi('Pmenu',               { fg = colors.black, bg = colors.beige2 });
+hi('PmenuSel',            { fg = colors.black, bg = colors.orange2 });
+hi('PmenuThumb',          { bg = colors.grey1 });
 
-hi('Type',            { fg = colors.copper });
-hi('Statement',       { fg = colors.steel });
-hi('Keyword',         { fg = colors.steel });
-hi('Constant',        { fg = colors.orange2 });
+hi('String',              { fg = colors.grey1 });
+hi('Character',           { fg = colors.grey1 });
+hi('Number',              { fg = colors.orange1 });
+hi('Float',               { fg = colors.orange1 });
+hi('Boolean',             { fg = colors.orange2 });
 
-hi('Identifier',      { fg = colors.beige3 });
-hi('Function',        { fg = colors.orange1 });
+hi('Type',                { fg = colors.copper });
+hi('Statement',           { fg = colors.steel });
+hi('Keyword',             { fg = colors.steel });
+hi('Constant',            { fg = colors.orange2 });
 
-hi('CursorLine',      { bg = colors.black });
-hi('CursorLineNr',    { bg = colors.black });
-hi('CursorColumn',    { bg = colors.black });
+hi('Identifier',          { fg = colors.beige3 });
+hi('Function',            { fg = colors.orange1 });
 
-hi('@tag',            { fg = colors.steel });
-hi('@tag.attribute',  { fg = colors.copper });
+hi('Todo',                { fg = colors.purple, bg = colors.grey5 });
 
-hi('IblIndent',       { fg = colors.grey4 });
-hi('IblWhitespace',   { fg = colors.grey4 });
-hi('IblScope',        { fg = colors.grey3 });
+hi('CursorLine',          { bg = colors.black });
+hi('CursorLineNr',        { fg = colors.orange1, bg = colors.black });
+hi('CursorColumn',        { bg = colors.black });
+
+hi('@tag',                { fg = colors.steel });
+hi('@tag.attribute',      { fg = colors.copper });
+hi('@string.special.url', { underline = false });
+
+hi('IblIndent',           { fg = colors.grey4 });
+hi('IblWhitespace',       { fg = colors.grey4 });
+hi('IblScope',            { fg = colors.grey3 });
