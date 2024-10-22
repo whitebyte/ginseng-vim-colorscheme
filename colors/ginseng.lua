d8af4f72ef4f4bcb95d51a24f8030ac295f0a9c3
Anton Pastukhov <caffeine9999@mailbox.org>
2024-10-22 15:34:34 +0300

    amend

--- colors/ginseng.lua
+++ colors/ginseng.lua
@@ -90,7 +90,7 @@ hi('CursorLine',          { bg = colors.black });
 hi('CursorLineNr',        { fg = colors.orange1, bg = colors.black });
 hi('CursorColumn',        { bg = colors.black });
 
-hi('VertSplit',           { fg = colors.grey3, bg = colors.grey3 });
+hi('VertSplit',           { fg = colors.grey3 });
 hi('WinSeparator',        { link = 'VertSplit' })
 
 hi('DiffAdd',             { bg = colors.green });
