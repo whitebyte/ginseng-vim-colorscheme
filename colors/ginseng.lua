7482e2e7b6aa9918623d41b2de36a91bad05e636
Anton Pastukhov <caffeine9999@mailbox.org>
2025-04-13 19:37:45 +0300

    nope

--- colors/ginseng.lua
+++ colors/ginseng.lua
@@ -157,7 +157,7 @@ hi('netrwPlain',          { fg = colors.beige2 });
 hi('netrwDir',            { fg = colors.orange1 });
 hi('netrwLink',           { fg = colors.grey1 });
 hi('netrwSymLink',        { fg = colors.grey1 });
-hi('netrwExe',            { fg = colors.beige1 });
+hi('netrwExe',            { fg = colors.white });
 hi('netrwComment',        { link = "Comment" });
 
 -- LSP
